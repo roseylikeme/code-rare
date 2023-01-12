0fba13ab905980e62afa26f993b75d93ed28498d
Roselene Gabun <roselenegabun@gmail.com>
2023-01-12 10:35:04 -0800

    Registration JS done

--- registration.js
+++ registration.js
@@ -6,36 +6,46 @@
 window.addEventListener('load', function () {
   this.document.getElementById("register").onclick = () => {
     // Values of Input Fields
-    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
     const pw = document.getElementById("psw").value;
     const confpw = document.getElementById("psw-repeat").value;
     // const form = document.getElementById("register-form");
     const msg = document.getElementById("msg")
     msg.innerHTML = ""
 
-    const isEmpty = (!(email && pw && confpw))
+    const isEmpty = (!(username && pw && confpw))
     if (!isEmpty && (pw === confpw)) {
       console.log("All fields were successfully filled.")
-
-      let userData = {
-        email: email,
+      let getData = {
+        username: username,
         password: pw
       }
 
       fetch("https://microbloglite.herokuapp.com/api/users",
-        {
+      {
           method: "POST",
           headers: { "Content-type": "application/json" },
           body: JSON.stringify(getData)
-        })
-        .then(response => response.json())
-        .then(json => {
+      })
+      .then(response => {
+          if(!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json()
+      })
+      .then(json => {
           setTimeout(
-            function () {
-              window.location.assign("index.html");
-            }, 5000);
-          msg.innerHTML = "User created successfully, Redirecting to Log In page"
-        });
+              function () {
+                  window.location.assign("index.html");
+              }, 5000);
+         msg.innerHTML = "User created successfully, Redirecting to Log In page"
+  
+      })
+      .catch(error => {
+        msg.innerHTML = "Something went wrong... check console."
+          console.error("Error:", error);
+      });
+  
     } 
     else {
       msg.innerHTML = "Something went wrong... check console."
