c2ea5cc88fde51d3f182b2b95ee08a825fa2c500
Roselene Gabun <roselenegabun@gmail.com>
2023-06-20 03:07:53 -0700

    no more profile pic icon

--- scripts/posts.js
+++ scripts/posts.js
@@ -14,58 +14,14 @@ window.addEventListener("load", function () {
   };
 });
 
-// Add more posts when scrolling
-window.addEventListener("scroll", () => {
-  if (
-    window.scrollY + window.innerHeight >=
-    document.documentElement.scrollHeight
-  ) {
-    displayUserPost();
-  }
-});
+// window.addEventListener("scroll", handleScroll);
 
-// Creates the cards
-function displayCard(data) {
-  document.getElementById("divForPost").innerHTML += `
-    <div class="row">
-    <div class="card mb-3" style="max-width: auto;">
-        <div class="card-body">
-            <h4 class="card-title"> <span><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-            </svg></span> @${
-              data.username
-            } <span style="color: #999999; font-size: x-small;"> Posted On: ${monthDayYear(
-    data.createdAt
-  )}</span></h4>
-            <p class="card-text">${data.text}</p>
-        </div>
-        <div class="card-footer bg-transparent">
-            <div class="btn-group" role="group" aria-label="Basic mixed styles example" style="width: 100%;">
-                <button type="button" class="btn likeBtn" value="${
-                  data._id
-                }" style="width: 33.3%;">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            fill="currentColor" class="bi bi-hand-thumbs-up"
-                            viewBox="0 0 16 16">
-                            <path
-                                d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                        </svg>
-                        ${data.likes.length}
-                    </span>
-                </button>
-                <button type="button" class="btn deleteBtn" style="width: 33.3%;">
-                    <span>
-                        <img src="images/deleteBtn.svg" alt="Delete Button" width="16" height="16">
-                    </span>
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-    `;
+
+function loginData() {
+  let loginData = getLoginData();
+  return loginData
 }
+
 // Displays the posts + Grabs from the API
 function displayUserPost() {
   let loginData = getLoginData();
@@ -117,6 +73,144 @@ function postBtnOnClick() {
     }
   });
 }
+
+function displayCard(data) {
+  const cardContainer = createCardContainer();
+  const card = createCard();
+  const cardBody = createCardBody();
+  const cardTitle = createCardTitle(data.username, data.createdAt);
+  const cardText = createCardText(data.text);
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer", "bg-transparent");
+
+  const btnGroup = document.createElement("div");
+  btnGroup.classList.add("btn-group");
+  btnGroup.setAttribute("role", "group");
+  btnGroup.setAttribute("aria-label", "Basic mixed styles example");
+  btnGroup.style.width = "100%";
+
+  const likeButton = createLikeButton(data.likes, data._id, btnGroup);
+  const deleteButton = createDeleteButton();
+
+  // Append elements
+  btnGroup.appendChild(likeButton);
+  btnGroup.appendChild(deleteButton);
+  cardFooter.appendChild(btnGroup);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  card.appendChild(cardBody);
+  card.appendChild(cardFooter);
+  cardContainer.appendChild(card);
+  document.getElementById("divForPost").appendChild(cardContainer);
+}
+
+function createLikeButton(likes, postId, btnGroup) {
+  const likeButton = document.createElement("button");
+  likeButton.classList.add("btn", "likeBtn");
+  likeButton.setAttribute("type", "button");
+  likeButton.setAttribute("value", postId);
+  likeButton.style.width = "33.3%";
+
+  if (likes.includes(loginData().userId)) {
+    likeButton.classList.add("liked");
+  }
+
+  likeButton.innerHTML = `
+    <span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+        fill="currentColor" class="bi bi-hand-thumbs-up"
+        viewBox="0 0 16 16">
+        <path
+            d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+    </svg>
+    </span> ${likes.length}`;
+
+  likeButton.addEventListener("click", (event) => {
+    const postIdValue = {
+      postId: postId,
+    };
+
+    fetch(API_BASE_URL + "/api/likes", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${loginData().token}`,
+      },
+      body: JSON.stringify(postIdValue),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  return likeButton;
+}
+
+// Helper functions for creating elements
+function createCardContainer() {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("row");
+  return cardContainer;
+}
+
+function createCard() {
+  const card = document.createElement("div");
+  card.classList.add("card", "mb-3");
+  card.style.maxWidth = "auto";
+  return card;
+}
+
+function createCardBody() {
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  return cardBody;
+}
+
+function createCardTitle(username, createdAt) {
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.innerText = `@${username}`;
+  const cardSubtitle = document.createElement("h6");
+  cardSubtitle.classList.add("card-subtitle", "mb-2", "text-muted");
+  cardSubtitle.innerText = `${monthDayYear(createdAt)}`;
+  cardTitle.appendChild(cardSubtitle);
+  return cardTitle;
+}
+
+function createCardText(text) {
+  const cardText = document.createElement("p");
+  cardText.classList.add("card-text");
+  cardText.innerText = text;
+  return cardText;
+}
+
+function createDeleteButton() {
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("btn", "deleteBtn");
+  deleteButton.setAttribute("type", "button");
+  deleteButton.innerHTML = `
+    <span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+        fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path
+            d="M4.5 5.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm1.5 0a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7zm3 0a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" />
+        <path
+            d="M1.5 3.5a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 0 1h-12a.5.5 0 0 1-.5-.5zm2.297-.357A.5.5 0 0 0 3 3.5h1v9a.5.5 0 0 0 .854.354l1.647-1.237h3.005l1.647 1.237a.5.5 0 0 0 .854-.354v-9h1a.5.5 0 0 0 .5-.5.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5.5.5 0 0 0 .5.5z" />
+    </svg>
+    </span> Delete`;
+
+  deleteButton.addEventListener("click", (event) => {
+    console.log("Delete button clicked");
+    // Add delete logic here
+  });
+
+  return deleteButton;
+}
+
 
 // Date Converter
 function monthDayYear(date) {
