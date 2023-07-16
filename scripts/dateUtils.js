817d6c08d51ab358a6c662e17b5fb4852fb736a6
Roselene Gabun <roselenegabun@gmail.com>
2023-07-16 15:14:24 -0700

    moved all shared funcs to diff file

--- scripts/dateUtils.js
+++
@@ -1,5 +0,0 @@
-function formatDateWithTime(dateString) {
-    const options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
-    const date = new Date(dateString);
-    return date.toLocaleDateString(undefined, options);
-}
